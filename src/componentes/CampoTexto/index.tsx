d519fa21ef1bb2336aa9adbe4493a386c1f53e14
Uryel Farias <uryel.farias@gmail.com>
2024-02-06 23:39:14 -0300

    Definindo os tipos no componente CampoTexto

--- src/componentes/CampoTexto/index.tsx
+++ src/componentes/CampoTexto/index.tsx
@@ -9,23 +9,23 @@ interface CampoTextoProps {
 
 }
 
-const CampoTexto = (props: CampoTextoProps) => {
+const CampoTexto = ({ aoAlterado, label, obrigatorio, placeholder, valor }  : CampoTextoProps) => {
 
-    const placeholderModificada = `${props.placeholder}...` 
+    const placeholderModificada = `${placeholder}...` 
 
     const aoDigitado = (evento: React.ChangeEvent<HTMLInputElement>) => {
-        props.aoAlterado(evento.target.value)
+        aoAlterado(evento.target.value)
     }
 
     return (
         <div className="campo-texto">
             <label>
-                {props.label}
+                {label}
             </label>
             <input 
-                value={props.valor} 
+                value={valor} 
                 onChange={aoDigitado} 
-                required={props.obrigatorio} 
+                required={obrigatorio} 
                 placeholder={placeholderModificada}
             />
         </div>
