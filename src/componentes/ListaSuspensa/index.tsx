4a2596a70f2fb06969d83bb6078fd84406a4f640
Uryel Farias <uryel.farias@gmail.com>
2024-02-08 23:52:42 -0300

    Migrando o componente lista suspença, primeira parte.

--- src/componentes/ListaSuspensa/index.tsx
+++ src/componentes/ListaSuspensa/index.tsx
@@ -1,10 +1,19 @@
 import './ListaSuspensa.css'
 
-const ListaSuspensa = (props) => {
+interface ListaSuspensaProps {
+    aoAlterado: (valor: string) => void
+    required: boolean
+    valor: string
+}
+
+const ListaSuspensa = (props: ListaSuspensaProps) => {
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+            <select onChange={evento => 
+                props.aoAlterado(evento.target.value)} 
+                required={props.required} 
+                value={props.valor}>
                 <option value=""></option>
                 {props.itens.map(item => {
                     return <option key={item}>{item}</option>
