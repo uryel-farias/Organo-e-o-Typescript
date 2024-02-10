3d2125589cfd817046f778b728a7ffd146bae931
Uryel Farias <uryel.farias@gmail.com>
2024-02-09 23:31:16 -0300

    Aprendendo sobre regras do tsconfig.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!); // a esclamaçã garante que o getElementeById vai retornar alguma coisa.
 root.render(
   <React.StrictMode>
     <App />
